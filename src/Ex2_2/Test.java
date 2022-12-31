355ba079b94dba46d33968c127504f4fa238faf9
NickBres <70432147+NickBres@users.noreply.github.com>
2022-12-31 11:18:37 +0200

    first commit for second part

---
+++ src/Ex2_2/Test.java
@@ -0,0 +1,57 @@
+package Ex2_2;
+
+import org.junit.Test;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.logging.Logger;
+
+public class Tests {
+    public static final Logger logger = LoggerFactory.getLogger(Tests.class);
+
+    @Test
+    public void partialTest() {
+        CustomExecutor customExecutor = new CustomExecutor();
+        var task = Task.createTask(() -> {
+            int sum = 0;
+            for (int i = 1; i <= 10; i++) {
+                sum += i;
+            }
+            return sum;
+        }, TaskType.COMPUTATIONAL);
+        var sumTask = customExecutor.submit(task);
+        final int sum;
+        try {
+            sum = sumTask.get(1, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            throw new RuntimeException(e);
+        }
+        logger.info(() -> "Sum of 1 through 10 = " + sum);
+        Callable<Double> callable1 = () -> {
+            return 1000 * Math.pow(1.02, 5);
+        };
+        Callable<String> callable2 = () -> {
+            StringBuilder sb = new StringBuilder("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+            return sb.reverse().toString();
+        };
+        var priceTask = customExecutor.submit(() -> {
+            return 1000 * Math.pow(1.02, 5);
+        }, TaskType.COMPUTATIONAL);
+        var reverseTask = customExecutor.submit(callable2, TaskType.IO);
+        final Double totalPrice;
+        final String reversed;
+        try {
+            totalPrice = priceTask.get();
+            reversed = reverseTask.get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+        logger.info(() -> "Reversed String = " + reversed);
+        logger.info(() -> String.valueOf("Total Price = " + totalPrice));
+        logger.info(() -> "Current maximum priority = " +
+                customExecutor.getCurrentMax());
+        customExecutor.gracefullyTerminate();
+    }
+}
